49fb23e947e1c24cf13842e7f6376bb16a022907
Jayaselvan <aghakrt@outlook.com>
2021-04-02 17:24:38 +0530

    Comments added

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Creating a Cache Memory with 4 functions namely to get data, set data, get inverse and set inverse
 
 makeCacheMatrix <- function(x = matrix()) {
   m_i <- NULL
@@ -10,7 +10,9 @@ makeCacheMatrix <- function(x = matrix()) {
     m_i <- NULL
   }
   get <- function() x
+  # Setting inverse incase of inverse is calculated by cacheSolve function
   setinv <- function(mat_inv) m_i <<- mat_inv
+  # used to send inverse mat to cacheSolve function
   getinv <- function() m_i
   list(set = set, get = get,
        setinv = setinv,
@@ -18,7 +20,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## checking for cache for inverse, if not present calculated the inverse
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -27,8 +29,12 @@ cacheSolve <- function(x, ...) {
     message("getting cached data")
     return(m_i)
   }
+  # getting matrix from makeCacheMatrix function
   data <- x$get()
+  # Calculating inverse
+  # inv method is avaulable in matlib library
   m_i <- inv(data, ...)
+  #m_i <- solve(data, ...)
   x$setinv(m_i)
   m_i
 }
